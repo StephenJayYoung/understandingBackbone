5097df7730a264d51e050e1f00900f5e107f06e7
Stephen Young <stephenjayyoung@gmail.com>
2015-09-17 10:20:27 -0700

    added files and dev environment

---
+++ demo 2/js/app.js
@@ -0,0 +1,31 @@
+﻿(function($){
+    //everything within the backbone.view.extend is going to do the things
+  var ListView = Backbone.View.extend({
+    el: $('body'), // el attaches to existing element
+//
+    events: {
+      'click button#add': 'addItem',
+      'click button#delete': 'deleteItem'
+    },
+
+    initialize: function(){
+      _.bindAll(this, 'render', 'addItem'); // every function that uses 'this' as the current object should be in here
+
+      this.counter = 0  ; // total number of items added thus far
+      this.render();
+    },
+
+    render: function(){
+      $(this.el).append("<button id='add'>Add the thing</button>");
+      $(this.el).append("<button id='delete'>Delete the thing</button>");
+      $(this.el).append("<ul></ul>");
+    },
+
+    addItem: function(){
+      this.counter++;
+      $('ul', this.el).append("<li>Count"+this.counter+"</li>");
+    }
+  });
+
+  var listView = new ListView();
+})(jQuery);
