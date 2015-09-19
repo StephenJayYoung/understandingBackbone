56aae2db7f035182fab6587c5128a42149aeb9f6
Stephen Young <stephenjayyoung@gmail.com>
2015-09-18 17:15:43 -0700

    added hw9 stuff to app.js

--- demo 2/js/app.js
+++ demo 2/js/app.js
@@ -1,47 +1,68 @@
-﻿(function($){
-    //everything within the backbone.view.extend is going to do the things
-  var ListView = Backbone.View.extend({
-    el: $('body'), // el attaches to existing element
+﻿
+// This view displays a single task, showing its title, description, status,
+// creator, and assignee (if any). Each TaskView should include one or more
+// controls (e.g. a select or set of buttons) to change its state.Each task
+// view will be associated with exactly one task model, although a model may
+// have more than one view instance.
+var TaskView = Backbone.View.extend({
+  initialize: function(){
+    alert("the TaskView.");
+  }
+});
+var taskView = new TaskView();
+
+
+//You'll need a view with input fields for the user to fill in when creating a new task.
+// It should probably have both a create and cancel button. The location and format of
+// the view is up to you.
+var CreateTaskView = Backbone.View.extend({
+  initialize: function(){
+    alert("the TaskView.");
+  }
+});
+var createTaskView = new CreateTaskView();
+
+
+//This view should include multiple TaskViews, each showing one of the unassigned tasks
+//reated by any user. The view should include a "Create New Task" button which triggers
+// a CreateTaskView, allowing the user to create a new task.
+var UnassignedTaskView = Backbone.View.extend({
+  initialize: function(){
+    alert("the unassigned task View.");
+  }
+});
+var unassignedTaskView = new UnassignedTaskView();
+
+// This view should include multiple TaskViews, one for each task for which the user
+//  is either the creator or assignee.
+var UserTaskViews = Backbone.View.extend({
+  initialize: function(){
+    alert("the user task View.");
+  }
+});
+var userTaskViews = new UserTaskViews();
+
+//This view contains everything the user sees once logged in. It should include 
+//both an UnassignedTasksView for claiming or creating new tasks and a UserTasksView 
+//for tracking the status of all tasks associated with the user.
+//It should also include a "logout" button which replaces the UserView with the 
+//LoginView
 //
-    events: {
-      'click button#add': 'addItem',
-      'click button#delete': 'subtractItem',
-      'click button#clear': 'clearItem'
-    },
+var UserView = Backbone.View.extend({
+  initialize: function(){
+    alert("the user view.");
+  }
+});
+var userView = new UserView();
 
-    initialize: function(){
-      _.bindAll(this, 'render', 'addItem', 'subtractItem', 'setToZero'); // every function that uses 'this' as the current object should be in here
-
-      this.counter = 0  ; // total number of items added thus far
-      this.render();
-    },
-
-    render: function(){
-      $(this.el).append("<button id='add'>Add the thing</button>");
-      $(this.el).append("<button id='delete'>Subtract the thing</button>");
-       $(this.el).append("<button id='clear'>Set to Zero</button>");
-      $(this.el).append("<ul></ul>");
-    },
-
-    addItem: function(){
-      this.counter++;
-      $('ul', this.el).append("<li>Count"+this.counter+"</li>");
-    },
-
-    subtractItem: function(){
-      this.counter--;
-      $('ul', this.el).append("<li>Subtract"+this.counter+"</li>");
-    },
-
-    setToZero: function(){
-      this.counter = 0;
-      $('ul', this.el).append("<li>Zero"+this.counter+"</li>");
-    }
-
-
-
-
-  });
-
-  var listView = new ListView();
-})(jQuery);
+//This is the initial view shown when launching the app 
+//(i.e. opening the html file in a browser). It should let the user either 
+//log in by choosing from a list of known user names, or create a new user name. 
+//Feel free to add some form of user authentication later, but keep things simple for now.
+//
+var LoginView = Backbone.View.extend({
+  initialize: function(){
+    alert("the loginView view.");
+  }
+});
+var loginView = new LoginView();
